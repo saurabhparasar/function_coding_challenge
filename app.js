33c35bd0c571554bee2b969c129b6c1381949725
saurabhparasar <75613400+saurabhparasar@users.noreply.github.com>
2021-07-31 23:59:43 +0530

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const calcAverage = (value1, value2, value3) => (value1 + value2 + value3) / 3;
 const scoreDolhins = calcAverage(85, 54, 41);
 const scoreKoalas  = calcAverage(23, 34, 27);
 
-
+// test data 2
 
 const checkWinner = function(avgDolhins, avgKoalas)  {
     if(avgDolhins >= 2 * avgKoalas){
